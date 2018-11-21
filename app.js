23463aaf6f647f30f2e20d0967034885b5f1278f
Kylin93CN <375020612@qq.com>
2018-11-21 17:22:42 +0800

    fix app.js

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 'use strict';
 module.exports = app => {
-  app.beforeStart(async () => {
+  app.beforeStart(() => {
     // 应用会等待这个函数执行完成才启动
     app.cities = 'xxx';
-
     // 也可以通过以下方式来调用 Service
     // const ctx = app.createAnonymousContext();
     // app.cities = await ctx.service.cities.load();
--- app/controller/mysql.js
+++ app/controller/mysql.js
@@ -4,6 +4,7 @@ const Controller = require('egg').Controller;
 
 class MysqlController extends Controller {
   async index() {
+    console.log(`@@@@@@${this.app.cities}`);
     const ctx = this.ctx;
     const user = await ctx.service.mysql.getInfo();
     ctx.body = user;
